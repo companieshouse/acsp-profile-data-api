81cf42ab176fff45ff8f437a7dfaf541cb5fa577
cgregoryCH <84070107+cgregoryCH@users.noreply.github.com>
2024-08-27 16:07:37 +0100

    DSND-2943: Implement full profile high level business processing (#5)

---
+++ src/main/java/uk/gov/companieshouse/acspprofile/api/service/AcspGetProcessor.java
@@ -0,0 +1,39 @@
+package uk.gov.companieshouse.acspprofile.api.service;
+
+import static uk.gov.companieshouse.acspprofile.api.AcspProfileApplication.NAMESPACE;
+
+import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.acspprofile.api.exception.NotFoundException;
+import uk.gov.companieshouse.acspprofile.api.logging.DataMapHolder;
+import uk.gov.companieshouse.acspprofile.api.mapper.GetMapper;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+@Component
+public class AcspGetProcessor implements GetProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
+    private final Service acspService;
+    private final GetMapper acspGetmapper;
+
+    public AcspGetProcessor(Service acspService, GetMapper acspGetmapper) {
+        this.acspService = acspService;
+        this.acspGetmapper = acspGetmapper;
+    }
+
+    @Override
+    public Object getProfile(String acspNumber) {
+        // not implemented
+        return null;
+    }
+
+    @Override
+    public Object getFullProfile(String acspNumber) {
+        return acspGetmapper.mapFullProfile(
+                acspService.findAcsp(acspNumber)
+                        .orElseGet(() -> {
+                            LOGGER.info("ACSP profile not found", DataMapHolder.getLogMap());
+                            throw new NotFoundException("ACSP profile not found");
+                        }));
+    }
+}
---
+++ src/test/java/uk/gov/companieshouse/acspprofile/api/service/AcspGetProcessorTest.java
@@ -0,0 +1,82 @@
+package uk.gov.companieshouse.acspprofile.api.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.acspprofile.api.exception.NotFoundException;
+import uk.gov.companieshouse.acspprofile.api.mapper.AcspGetMapper;
+import uk.gov.companieshouse.acspprofile.api.model.AcspProfileDocument;
+
+@ExtendWith(MockitoExtension.class)
+class AcspGetProcessorTest {
+
+    private static final String ACSP_NUMBER = "12345678";
+
+    @InjectMocks
+    private AcspGetProcessor getProcessor;
+    @Mock
+    private AcspService service;
+    @Mock
+    private AcspGetMapper mapper;
+
+    @Mock
+    private AcspProfileDocument document;
+    @Mock
+    private Object expected;
+
+    @Test
+    void shouldNotGetProfileWhenNotImplemented() {
+        // given
+
+        // when
+        Object actual = getProcessor.getProfile(ACSP_NUMBER);
+
+        // then
+        assertNull(actual);
+        verifyNoInteractions(service);
+        verifyNoInteractions(mapper);
+    }
+
+    @Test
+    void shouldGetFullProfile() {
+        // given
+        when(service.findAcsp(any())).thenReturn(Optional.of(document));
+        when(mapper.mapFullProfile(any())).thenReturn(expected);
+
+        // when
+        Object actual = getProcessor.getFullProfile(ACSP_NUMBER);
+
+        // then
+        assertEquals(expected, actual);
+        verify(service).findAcsp(ACSP_NUMBER);
+        verify(mapper).mapFullProfile(document);
+    }
+
+    @Test
+    void shouldThrowNotFoundExceptionWhenGetFullProfileEmptyOptional() {
+        // given
+        when(service.findAcsp(any())).thenReturn(Optional.empty());
+
+        // when
+        Executable actual = () -> getProcessor.getFullProfile(ACSP_NUMBER);
+
+        // then
+        Exception exception = assertThrows(NotFoundException.class, actual);
+        assertEquals("ACSP profile not found", exception.getMessage());
+        verify(service).findAcsp(ACSP_NUMBER);
+        verifyNoMoreInteractions(mapper);
+    }
+}
