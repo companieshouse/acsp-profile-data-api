b1ef2887f47d918f51b4a61f4638bc849bc63419
cgregoryCH <cgregory@companieshouse.gov.uk>
2024-08-29 16:57:11 +0100

    DSND-2943: Improve logging

--- src/main/java/uk/gov/companieshouse/acspprofile/api/auth/FullProfileAuthInterceptor.java
+++ src/main/java/uk/gov/companieshouse/acspprofile/api/auth/FullProfileAuthInterceptor.java
@@ -34,19 +34,19 @@ public class FullProfileAuthInterceptor implements HandlerInterceptor {
         String ericIdentityType = request.getHeader(ERIC_IDENTITY_TYPE);
 
         if (StringUtils.isBlank(ericIdentity)) {
-            LOGGER.error("Request received without eric identity", DataMapHolder.getLogMap());
+            LOGGER.error("Eric identity is blank", DataMapHolder.getLogMap());
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
 
         if (!API_KEY_IDENTITY_TYPE.equalsIgnoreCase(ericIdentityType)) {
-            LOGGER.error("Request received without correct eric identity type", DataMapHolder.getLogMap());
+            LOGGER.error("Incorrect eric identity type", DataMapHolder.getLogMap());
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
 
         if (!isKeySensitiveDataAuthorised(request)) {
-            LOGGER.error("Supplied key does not have sufficient privileges", DataMapHolder.getLogMap());
+            LOGGER.error("Key is not authorised", DataMapHolder.getLogMap());
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return false;
         }
