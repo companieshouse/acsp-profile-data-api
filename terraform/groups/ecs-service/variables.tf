bcd2234625fe8c90c5600870c2da9f743968ae4c
cgregoryCH <84070107+cgregoryCH@users.noreply.github.com>
2024-10-21 16:45:42 +0100

    DSND-3137: Increase tasks, vCPU and Memory resources for production (#17)

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -28,35 +28,39 @@ variable "docker_registry" {
 # Service performance and scaling configs
 # ------------------------------------------------------------------------------
 variable "desired_task_count" {
-  type = number
+  type        = number
   description = "The desired ECS task count for this service"
-  default = 1 # defaulted low for dev environments, override for production
+  default     = 1 # defaulted low for dev environments, override for production
 }
-variable "required_cpus" {
-  type = number
-  description = "The required cpu resource for this service. 1024 here is 1 vCPU"
-  default = 256 # defaulted low for dev environments, override for production
+variable "min_task_count" {
+  type        = number
+  description = "The minimum number of tasks for this service."
+  default     = 1
 }
-variable "required_memory" {
-  type = number
-  description = "The required memory for this service"
-  default = 512 # defaulted low for node service in dev environments, override for production
-}
-variable "eric_cpus" {
-  type = number
-  description = "The required cpu resource for eric. 1024 here is 1 vCPU"
-  default = 256
-}
-variable "eric_memory" {
-  type = number
-  description = "The required memory for eric"
-  default = 512
-}
-
 variable "max_task_count" {
   type        = number
   description = "The maximum number of tasks for this service."
-  default     = 10
+  default     = 5
+}
+variable "required_cpus" {
+  type        = number
+  description = "The required cpu resource for this service. 1024 here is 1 vCPU"
+  default     = 256 # defaulted low for dev environments, override for production
+}
+variable "required_memory" {
+  type        = number
+  description = "The required memory for this service"
+  default     = 512 # defaulted low for node service in dev environments, override for production
+}
+variable "eric_cpus" {
+  type        = number
+  description = "The required cpu resource for eric. 1024 here is 1 vCPU"
+  default     = 256
+}
+variable "eric_memory" {
+  type        = number
+  description = "The required memory for eric"
+  default     = 512
 }
 
 variable "use_fargate" {
@@ -85,7 +89,7 @@ variable "service_scaledown_schedule" {
   # Typically used to stop all tasks in a service to save resource costs overnight.
   # E.g. a value of '55 19 * * ? *' would be Mon-Sun 7:55pm.  An empty string indicates that no schedule should be created.
 
-  default     = ""
+  default = ""
 }
 variable "service_scaleup_schedule" {
   type        = string
@@ -93,7 +97,7 @@ variable "service_scaleup_schedule" {
   # Typically used to start all tasks in a service after it has been shutdown overnight.
   # E.g. a value of '5 6 * * ? *' would be Mon-Sun 6:05am.  An empty string indicates that no schedule should be created.
 
-  default     = ""
+  default = ""
 }
 
 # ----------------------------------------------------------------------
